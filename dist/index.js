bf88e53add3d18cdaee3df32fece50f634e45910
Yanks Yoon <yangsoo.yoon@canonical.com>
2024-12-29 15:29:59 +0000

    insttal openstack within lxc vm

--- dist/index.js
+++ dist/index.js
@@ -25667,52 +25667,50 @@ var __importStar = (this && this.__importStar) || (function () {
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.run = run;
 const core = __importStar(__nccwpck_require__(7484));
-const wait_1 = __nccwpck_require__(910);
+const exec = __importStar(__nccwpck_require__(5236));
+var INPUT_OPTIONS;
+(function (INPUT_OPTIONS) {
+    INPUT_OPTIONS["FLAVOR"] = "flavor";
+    INPUT_OPTIONS["CORES"] = "cores";
+    INPUT_OPTIONS["MEMORY"] = "mememory";
+    INPUT_OPTIONS["DISK"] = "disk";
+})(INPUT_OPTIONS || (INPUT_OPTIONS = {}));
+const DEFAULT_FLAVOR = '24.04';
+const DEFAULT_NUM_CORES = '6';
+const DEFAULT_SIZE_MEM = '32GiB';
+const DEFAULT_SIZE_DISK = '50GB';
+const OPENSTACK_VM_NAME = 'openstack';
+const UBUNTU_UID = '1000';
+const SNAP_DAEMON_GID = '584788';
+const EXEC_COMMAND_UBUNTU_USER = `lxc exec ${OPENSTACK_VM_NAME} --user ${UBUNTU_UID} --group ${SNAP_DAEMON_GID} --`;
+const SUNBEAM_ADMIN_CLOUD_NAME = 'sunbeam-admin';
+const OPENSTACK_CLOUDS_YAML_PATH = '~/.config/openstack/clouds.yaml';
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 async function run() {
     try {
-        const ms = core.getInput('milliseconds');
-        // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-        core.debug(`Waiting ${ms} milliseconds ...`);
-        // Log the current timestamp, wait, then log the new timestamp
-        core.debug(new Date().toTimeString());
-        await (0, wait_1.wait)(parseInt(ms, 10));
-        core.debug(new Date().toTimeString());
-        // Set outputs for other workflow steps to use
-        core.setOutput('time', new Date().toTimeString());
+        const flavor = core.getInput(INPUT_OPTIONS.FLAVOR) || DEFAULT_FLAVOR;
+        const cores = core.getInput(INPUT_OPTIONS.CORES) || DEFAULT_NUM_CORES;
+        const mem = core.getInput(INPUT_OPTIONS.MEMORY) || DEFAULT_SIZE_MEM;
+        const disk = core.getInput(INPUT_OPTIONS.DISK) || DEFAULT_SIZE_DISK;
+        await exec.exec('sudo lxd init', ['--auto']);
+        await exec.exec(`lxc launch ubuntu:${flavor} ${OPENSTACK_VM_NAME} \
+      --vm \
+      -d root,size=${disk} \
+      -c limits.cpu=${cores} \
+      -c limits.memory=${mem}`);
+        await exec.exec(`${EXEC_COMMAND_UBUNTU_USER} sudo snap install openstack --channel 2024.1/beta`);
+        await exec.exec(`${EXEC_COMMAND_UBUNTU_USER} sunbeam prepare-node-script | bash -x`);
+        await exec.exec(`${EXEC_COMMAND_UBUNTU_USER} sunbeam cluster bootstrap --accept-defaults`);
+        await exec.exec(`${EXEC_COMMAND_UBUNTU_USER} sunbeam cloud-config -a > ${OPENSTACK_CLOUDS_YAML_PATH}`);
     }
     catch (error) {
         // Fail the workflow run if an error occurs
         if (error instanceof Error)
             core.setFailed(error.message);
     }
-}
-
-
-/***/ }),
-
-/***/ 910:
-/***/ ((__unused_webpack_module, exports) => {
-
-"use strict";
-
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.wait = wait;
-/**
- * Wait for a number of milliseconds.
- * @param milliseconds The number of milliseconds to wait.
- * @returns {Promise<string>} Resolves with 'done!' after the wait is over.
- */
-async function wait(milliseconds) {
-    return new Promise(resolve => {
-        if (isNaN(milliseconds)) {
-            throw new Error('milliseconds not a number');
-        }
-        setTimeout(() => resolve('done!'), milliseconds);
-    });
 }
 
 
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,24 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import * as exec from '@actions/exec'
+
+enum INPUT_OPTIONS {
+  FLAVOR = 'flavor',
+  CORES = 'cores',
+  MEMORY = 'mememory',
+  DISK = 'disk'
+}
+
+const DEFAULT_FLAVOR = '24.04'
+const DEFAULT_NUM_CORES = '6'
+const DEFAULT_SIZE_MEM = '32GiB'
+const DEFAULT_SIZE_DISK = '50GB'
+const OPENSTACK_VM_NAME = 'openstack'
+
+const UBUNTU_UID = '1000'
+const SNAP_DAEMON_GID = '584788'
+const EXEC_COMMAND_UBUNTU_USER = `lxc exec ${OPENSTACK_VM_NAME} --user ${UBUNTU_UID} --group ${SNAP_DAEMON_GID} --`
+const SUNBEAM_ADMIN_CLOUD_NAME = 'sunbeam-admin'
+const OPENSTACK_CLOUDS_YAML_PATH = '~/.config/openstack/clouds.yaml'
 
 /**
  * The main function for the action.
@@ -7,18 +26,29 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    const flavor: string = core.getInput(INPUT_OPTIONS.FLAVOR) || DEFAULT_FLAVOR
+    const cores: string =
+      core.getInput(INPUT_OPTIONS.CORES) || DEFAULT_NUM_CORES
+    const mem: string = core.getInput(INPUT_OPTIONS.MEMORY) || DEFAULT_SIZE_MEM
+    const disk: string = core.getInput(INPUT_OPTIONS.DISK) || DEFAULT_SIZE_DISK
+    await exec.exec('sudo lxd init', ['--auto'])
+    await exec.exec(`lxc launch ubuntu:${flavor} ${OPENSTACK_VM_NAME} \
+      --vm \
+      -d root,size=${disk} \
+      -c limits.cpu=${cores} \
+      -c limits.memory=${mem}`)
+    await exec.exec(
+      `${EXEC_COMMAND_UBUNTU_USER} sudo snap install openstack --channel 2024.1/beta`
+    )
+    await exec.exec(
+      `${EXEC_COMMAND_UBUNTU_USER} sunbeam prepare-node-script | bash -x`
+    )
+    await exec.exec(
+      `${EXEC_COMMAND_UBUNTU_USER} sunbeam cluster bootstrap --accept-defaults`
+    )
+    await exec.exec(
+      `${EXEC_COMMAND_UBUNTU_USER} sunbeam cloud-config -a > ${OPENSTACK_CLOUDS_YAML_PATH}`
+    )
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
